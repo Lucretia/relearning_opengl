a6348a81d6c0b30e36a5051f1e81a53d8370dc03
Luke A. Guest <laguest@archeia.com>
2020-09-28 19:42:15 +0100

    Completed first part of tutorial, now have a blue/green window.

--- learnopengl.com/hello_window/src/hello_window.cpp
+++ learnopengl.com/hello_window/src/hello_window.cpp
@@ -1,5 +1,10 @@
+#include <iostream>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
 int main(int argc, char* argv[])
 {
@@ -8,5 +13,60 @@ int main(int argc, char* argv[])
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    const int windowWidth  = 1280;
+    const int windowHeight = 1024;
+
+    GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "LearnOpenGL - Hello Window", NULL, NULL);
+
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+
+        glfwTerminate();
+
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // This cannot be any earlier otherwise it fails.
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialise GLAD" << std::endl;
+
+        return -1;
+    }
+
+    // glViewport(0, 0, windowWidth, windowHeight);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        // Rendering.
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Check/call events and then swap the render buffers.
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    } // Render loop.
+
+    glfwTerminate();
+
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
