b70f73b6ffafa7213d9d9357bb28667c9568bfcc
Luke A. Guest <laguest@archeia.com>
2020-09-29 15:49:06 +0100

    Change triangle to square using 2 triangles and element buffers.

--- learnopengl.com/hello_triangle/src/hello_triangle.cpp
+++ learnopengl.com/hello_triangle/src/hello_triangle.cpp
@@ -84,21 +84,32 @@ int main(int argc, char* argv[])
 
     // Triangle.
     const float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+         0.5f,  0.5f, 0.0f, // Top right.
+         0.5f, -0.5f, 0.0f, // Bottom right.
+        -0.5f, -0.5f, 0.0f, // Bottom left.
+        -0.5f,  0.5f, 0.0f  // Top left.
+    };
+
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
     };
 
     unsigned int vao;
     unsigned int vbo;
+    unsigned int ebo;
 
     glGenVertexArrays(1, &vao);
     glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
 
     // Have to bind the VAO before the VBO, then set up the vertex attributes.
     glBindVertexArray(vao);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
     glEnableVertexAttribArray(0);
@@ -116,7 +127,9 @@ int main(int argc, char* argv[])
 
         glUseProgram(shaderProgram);
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        glBindVertexArray(0);
 
         // Check/call events and then swap the render buffers.
         glfwSwapBuffers(window);
