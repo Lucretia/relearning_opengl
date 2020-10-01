c72732e7e16a971d638f06eef993ad688b0489bb
Luke A. Guest <laguest@archeia.com>
2020-10-01 17:36:15 +0100

    Add textures example.

---
+++ learnopengl.com/04_textures/src/textures.cpp
@@ -0,0 +1,185 @@
+#include <iostream>
+#include <cmath>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <utils/shader.hpp>
+#include <stb_image/stb_image.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void keyboard_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+// Triangle.
+const float vertices[] = {
+    // Vertices             Colours             Texture coords
+     0.5f,  0.5f, 0.0f,     1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // Top right.
+     0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // Bottom right.
+    -0.5f, -0.5f, 0.0f,     0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // Bottom left.
+    -0.5f,  0.5f, 0.0f,     1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // Top left.
+};
+
+unsigned int indices[] = {
+    0, 1, 3,
+    1, 2, 3
+};
+
+#define PATH    "../learnopengl.com/04_textures"
+
+int main(int argc, char* argv[])
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    const int windowWidth  = 1280;
+    const int windowHeight = 1024;
+
+    GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "LearnOpenGL - Textures", NULL, NULL);
+
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+
+        glfwTerminate();
+
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetKeyCallback(window, keyboard_callback);
+
+    // This cannot be any earlier otherwise it fails.
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialise GLAD" << std::endl;
+
+        return -1;
+    }
+
+    int numAttributes;
+
+    glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &numAttributes);
+
+    std::cout << "Max. number of vertex attributes: " << numAttributes << std::endl;
+
+    // Shaders.
+    Shader ourShader(PATH "/src/shader.vs", PATH "/src/shader.fs");
+
+    unsigned int vao;
+    unsigned int vbo;
+    unsigned int ebo;
+
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+
+    // Have to bind the VAO before the VBO, then set up the vertex attributes.
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), nullptr);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void*>(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    // glBindBuffer(GL_ARRAY_BUFFER, 0);
+    // glBindVertexArray(0);
+
+    unsigned int texture;
+
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width;
+    int height;
+    int numChannels;
+    const unsigned char* const data = stbi_load(PATH "/textures/container.jpg", &width, &height, &numChannels, 0);
+
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture: " << std::endl;
+    }
+
+    stbi_image_free(reinterpret_cast<void*>(const_cast<unsigned char *>(data)));
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // Rendering.
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ourShader.use();
+
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glBindVertexArray(vao);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        // Check/call events and then swap the render buffers.
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    } // Render loop.
+
+    glDeleteBuffers(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &ebo);
+
+    glfwTerminate();
+
+    return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void keyboard_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    static bool fillModeOn = true;
+
+    switch (key)
+    {
+        case GLFW_KEY_F:
+            if (action == GLFW_PRESS)
+                {
+                    if (fillModeOn)
+                    {
+                        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+                    }
+                    else
+                    {
+                        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+                    }
+
+                    fillModeOn = !fillModeOn;
+                }
+            break;
+
+        case GLFW_KEY_ESCAPE:
+            glfwSetWindowShouldClose(window, true);
+
+            break;
+    }
+}
+
