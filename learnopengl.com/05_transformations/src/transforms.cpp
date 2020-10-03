e3dc1286b854bd615ff8b30451305b94a903bbb4
Luke A. Guest <laguest@archeia.com>
2020-10-03 14:45:17 +0100

    Re-order the transforms.

--- learnopengl.com/05_transformations/src/transforms.cpp
+++ learnopengl.com/05_transformations/src/transforms.cpp
@@ -154,14 +154,7 @@ int main(int argc, char* argv[])
     ourShader.setInt("texture1", 0);
     ourShader.setInt("texture2", 1);
 
-    glm::mat4 trans = glm::mat4(1.0f);
-
-    trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
-    trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
-
     unsigned int transformLocation = glGetUniformLocation(ourShader.ID, "transform");
-
-    glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(trans));
 
     while (!glfwWindowShouldClose(window))
     {
@@ -174,6 +167,14 @@ int main(int argc, char* argv[])
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
         glBindVertexArray(vao);
+
+        glm::mat4 trans = glm::mat4(1.0f);
+
+        trans = glm::rotate(trans, static_cast<float>(glfwGetTime()), glm::vec3(0.0f, 0.0f, 1.0f));
+        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 1.0f));
+
+        glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(trans));
+
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // Check/call events and then swap the render buffers.
