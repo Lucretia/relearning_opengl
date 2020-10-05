6f24c1f386136133a92dca51b831fe21ed80f5ae
Luke A. Guest <laguest@archeia.com>
2020-10-05 10:54:13 +0100

    comment out texturing in the shader for now.

--- learnopengl.com/08_colours/src/shader.fs
+++ learnopengl.com/08_colours/src/shader.fs
@@ -3,12 +3,16 @@
 
 out vec4 FragColour;
 
-in  vec2 TexCoord;
+// in  vec2 TexCoord;
 
-uniform sampler2D texture1;
-uniform sampler2D texture2;
+// uniform sampler2D texture1;
+// uniform sampler2D texture2;
+
+uniform vec3 objectColour;
+uniform vec3 lightColour;
 
 void main()
 {
-    FragColour = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+    // FragColour = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+    FragColour = vec4(lightColour * objectColour, 1.0);
 }
